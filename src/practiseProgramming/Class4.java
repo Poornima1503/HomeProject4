eaa4306ab94674058fbe6d0df668768609dfe0c3
poornima S <poornii87@gmail.com>
2019-08-11 07:47:32 +0100

    HomeProject4

---
+++ src/practiseProgramming/Class4.java
@@ -0,0 +1,18 @@
+//Creating package
+package practiseProgramming;
+
+//Creating class
+public class Class4 {
+
+    /* Main function first prints my project count
+     * Main function second prints my class count
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my fourth project");
+        System.out.println("My fourth class created");
+    }
+
+//  Main function ends
+
+}
+//Class4 ends
